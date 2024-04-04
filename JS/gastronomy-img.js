87631cabcba30869d312632436731eee40db7689
bautista zuccatti <zuccattibautista@gmail.com>
2024-04-04 16:35:30 -0300

    elimino archivos inecesarios

--- JS/gastronomy-img.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var image = document.querySelector('#animation-gastronomy .gastronomia-ph');
-    image.style.left = '0'; // Posiciona la imagen en su posición original
-});
