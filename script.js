2b4cbca9e10da1f3ebdcaa7cada5ba3d8b807c11
bautista zuccatti <zuccattibautista@gmail.com>
2024-05-07 21:48:16 -0300

    agrego loader

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+
+    setTimeout(function() {
+        loader.style.transition = 'all 1s ease';
+        content.style.transition = 'all 1s ease';
+        loader.style.transform = 'translateY(-100%)';
+        content.classList.remove('hidden');
+    }, 2500); 
+});
