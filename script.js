27b2f275cce354b0bb0c6100d599b7c4331b1ad4
bautista zuccatti <zuccattibautista@gmail.com>
2024-05-08 00:27:58 -0300

    modifico el loader

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ window.addEventListener('load', function() {
     const loader = document.getElementById('loader');
     const content = document.getElementById('content');
 
+    document.body.classList.add('loading');
+
     setTimeout(function() {
         loader.style.transition = 'all 1s ease';
         content.style.transition = 'all 1s ease';
         loader.style.transform = 'translateY(-100%)';
         content.classList.remove('hidden');
-    }, 2500); 
+
+        
+        document.body.classList.remove('loading');
+    }, 2000); 
 });
